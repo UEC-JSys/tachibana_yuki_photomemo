68f484e4f61128940dc263d57afa994fbb649d6c
tachitachi15 <tachitachi15@gmail.com>
2020-11-04 17:26:39 +0900

    写真追加画面作成中

--- app/src/main/java/com/yuki/photomemo/AddPhotoActivity.kt
+++ app/src/main/java/com/yuki/photomemo/AddPhotoActivity.kt
@@ -2,10 +2,14 @@ package com.yuki.photomemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddPhotoActivity : AppCompatActivity() {
+
+    private val pickPhotoRequestCode = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
     }
+    val openButton: Button = findViewById(R.id.addPhotoOpenButton)
 }
