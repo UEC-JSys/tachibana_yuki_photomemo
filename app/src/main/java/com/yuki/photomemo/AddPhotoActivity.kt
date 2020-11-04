cf0b88850c37866a7cede623267db496a73d15ff
tachitachi15 <tachitachi15@gmail.com>
2020-11-04 17:46:38 +0900

    p46の実行　エラー無いけどレイアウトがおかしいのでデバッグする

--- app/src/main/java/com/yuki/photomemo/AddPhotoActivity.kt
+++ app/src/main/java/com/yuki/photomemo/AddPhotoActivity.kt
@@ -1,8 +1,11 @@
 package com.yuki.photomemo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class AddPhotoActivity : AppCompatActivity() {
 
@@ -10,6 +13,25 @@ class AddPhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
+
+        val openButton: Button = findViewById(R.id.addPhotoOpenButton)
+        openButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                setType("image/jpeg")
+            }
+            startActivityForResult(intent, pickPhotoRequestCode)
+        }
     }
-    val openButton: Button = findViewById(R.id.addPhotoOpenButton)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == pickPhotoRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.data?.let {
+                val imageView = findViewById<ImageView>(R.id.addPhotoImageView)
+                imageView.setImageURI(it)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/yuki/photomemo/MainActivity.kt
+++ app/src/main/java/com/yuki/photomemo/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.yuki.photomemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private val addActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddPhotoActivity::class.java)
+            startActivityForResult(intent, addActivityRequestCode)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
